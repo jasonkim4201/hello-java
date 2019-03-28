2fb34c9abe878f080a9cfbe867a2d5d56ef463f0
Jason Kim <jasonkim4201@gmail.com>
2019-03-28 11:53:46 -0400

    notes
    testing githib desktop version with my first java lines of code

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        String name = "Jason";
+        int age = 27;
+        System.out.println("Hello World!");
+        System.out.println("Wow very cool. Now use this and maybe you'll get a bit more familiar with  C#");
+        System.out.println("Hi Jason");
+        System.out.println(name + " is " + age + " years old.");
+    }
+}
